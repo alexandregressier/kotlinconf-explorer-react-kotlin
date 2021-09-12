bae3938ae3b0e11bbc9e78ba6e0175d36f77cceb
Alexandre Gressier <alexandre@gressier.dev>
2021-09-12 18:08:48 +0200

    Implement an alert that logs the video clicked

--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -1,7 +1,10 @@
+import kotlinx.browser.window
+import kotlinx.html.js.onClickFunction
 import react.Props
 import react.RBuilder
 import react.RComponent
 import react.State
+import react.dom.attrs
 import react.dom.p
 
 external interface VideoListProps : Props {
@@ -16,6 +19,9 @@ class VideoList : RComponent<VideoListProps, State>() {
         props.videos.forEach { video ->
             p {
                 key = "${video.id}"
+                attrs {
+                    onClickFunction = { window.alert("Clicked $video!")}
+                }
                 +"${video.speaker}: ${video.title}"
             }
         }
