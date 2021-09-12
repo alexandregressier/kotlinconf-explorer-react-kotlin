0ad22ead73cabe8c92ff447ae4197135417bce0e
Alexandre Gressier <alexandre@gressier.dev>
2021-09-12 19:21:02 +0200

    Implement the "Mark as watched/unwatched" button

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -5,26 +5,53 @@ import react.dom.h3
 
 external interface AppState : State {
     var currentVideo: Video?
+    var unwatchedVideos: List<Video>
+    var watchedVideos: List<Video>
 }
 @JsExport
 class App : RComponent<Props, AppState>() {
+
+    override fun AppState.init() {
+         unwatchedVideos = listOf(
+            KotlinVideo(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+            KotlinVideo(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+            KotlinVideo(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+        )
+         watchedVideos = listOf(
+            KotlinVideo(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+        )
+    }
 
     override fun RBuilder.render() {
         h1 { +"Hello, React & Kotlin/JS!" }
         div {
             h3 { +"Videos to watch" }
             videoList {
-                videos = unwatchedVideos
+                videos = state.unwatchedVideos
                 selectedVideo = state.currentVideo
                 onSelectVideo = { setState { currentVideo = it } }
             }
             h3 { +"Videos watched" }
             videoList {
-                videos = watchedVideos
+                videos = state.watchedVideos
                 selectedVideo = state.currentVideo
                 onSelectVideo = { setState { currentVideo = it } }
             }
         }
-        state.currentVideo?.let { videoPlayer { video = it } }
+        state.currentVideo?.let { currentVideo ->
+            videoPlayer {
+                video = currentVideo
+                unwatchedVideo = currentVideo in state.unwatchedVideos
+                onWatchedButtonPressed = { setState {
+                    if (video in state.unwatchedVideos) {
+                        unwatchedVideos -= video
+                        watchedVideos += video
+                    } else {
+                        watchedVideos -= video
+                        unwatchedVideos += video
+                    }
+                } }
+            }
+        }
     }
 }
--- src/main/kotlin/VideoPlayer.kt
+++ src/main/kotlin/VideoPlayer.kt
@@ -1,8 +1,9 @@
+import kotlinx.css.*
+import kotlinx.css.Color.Companion.lightGreen
+import kotlinx.css.Color.Companion.red
+import kotlinx.css.Display.block
 import kotlinx.css.Position.absolute
-import kotlinx.css.position
-import kotlinx.css.px
-import kotlinx.css.right
-import kotlinx.css.top
+import kotlinx.html.js.onClickFunction
 import react.Props
 import react.RBuilder
 import react.RComponent
@@ -11,10 +12,13 @@ import react.dom.attrs
 import react.dom.h3
 import react.dom.img
 import styled.css
+import styled.styledButton
 import styled.styledDiv
 
 external interface VideoPlayerProps : Props {
     var video: Video
+    var onWatchedButtonPressed: (Video) -> Unit
+    var unwatchedVideo: Boolean
 }
 @JsExport
 class VideoPlayer : RComponent<VideoPlayerProps, State>() {
@@ -27,6 +31,16 @@ class VideoPlayer : RComponent<VideoPlayerProps, State>() {
                 right = 10.px
             }
             h3 { props.video.run { +"$speaker: $title" } }
+            styledButton {
+                css {
+                    display = block
+                    backgroundColor = if (props.unwatchedVideo) lightGreen else red
+                }
+                attrs {
+                    onClickFunction = { props.onWatchedButtonPressed(props.video) }
+                }
+                +"Mark as ${if (!props.unwatchedVideo) "unwatched" else "watched"}"
+            }
             img {
                 attrs {
                     src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
